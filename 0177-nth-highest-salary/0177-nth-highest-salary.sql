9fbf1c98b1cdc6c4e670728bc24617f1fde9beec
Yasmeen Ragheb <105052150+yasmeenragheb@users.noreply.github.com>
2025-05-04 15:56:40 +0300

    Time: 421 ms (63.74%), Space: 0B (100%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -2,14 +2,12 @@ CREATE FUNCTION getNthHighestSalary(@N INT)
 RETURNS INT 
 AS
 BEGIN
-    DECLARE @result INT;
+return(
 
-    SELECT @result = salary
+    SELECT salary
     FROM (
         SELECT DISTINCT salary, DENSE_RANK() OVER (ORDER BY salary DESC) AS rank
         FROM Employee
     ) AS DistinctRanks
-    WHERE rank = @N;
-
-    RETURN @result;  -- Will return NULL if no such rank exists
+    WHERE rank = @N);
 END
